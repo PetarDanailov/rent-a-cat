fb0da425483a27f410f5e7d17d363efb5f30dcbc
PetarDanailov <danailovvpetar@gmail.com>
2024-11-24 12:42:42 +0200

    show cats from real time db

--- src/api/catsAPI.js
+++ src/api/catsAPI.js
@@ -1,4 +1,8 @@
-const url = ''
-async function getAll(){
-
+const url = 'https://rent-a-cat-da086-default-rtdb.europe-west1.firebasedatabase.app/cats'
+export async function getAll(){
+  const responce = await fetch(`${url}.json`);
+  const result = await responce.json();
+  
+  const cats = Object.keys(result).map(id => ({id, ...result[id]}));
+  return cats
 }
